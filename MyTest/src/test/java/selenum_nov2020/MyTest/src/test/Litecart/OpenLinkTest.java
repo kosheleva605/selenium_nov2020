99483abb519bd714cca42dde02f059bc0ba5195e
kosheleva605 <kosheleva605@yandex.ru>
2021-01-28 21:20:06 +0300

    Litecart MessagesTest

--- MyTest/src/test/java/selenum_nov2020/MyTest/src/test/Litecart/OpenLinkTest.java
+++ MyTest/src/test/java/selenum_nov2020/MyTest/src/test/Litecart/OpenLinkTest.java
@@ -1,5 +1,6 @@
 package selenum_nov2020.MyTest.src.test.Litecart;
 
+import org.checkerframework.checker.nullness.qual.Nullable;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -8,6 +9,7 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -21,6 +23,7 @@ public class OpenLinkTest {
     private WebDriver driver;
     private WebDriverWait wait;
 
+
     @Before
     public void start() {
         driver = new ChromeDriver();
@@ -30,7 +33,7 @@ public class OpenLinkTest {
     }
 
     @Test
-    public void shouldSwitchToNewLinkAndClose() throws InterruptedException {
+    public void shouldSwitchToNewLinkAndClose() {
         driver.get("http://localhost/litecart/admin/login.php");
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
@@ -47,15 +50,29 @@ public class OpenLinkTest {
         List<WebElement> links = table.findElements(By.cssSelector("a[target = '_blank']"));
         int num = links.size();
         for (int i = 0; i < num; i++) {
+            final Set<String> oldWindows1 = driver.getWindowHandles();
             links.get(i).click();
-            Thread.sleep(3000);
+            //Thread.sleep(3000);
+
+
+            String newWindow = (new WebDriverWait(driver, Duration.ofSeconds(50)))
+                    .until(new ExpectedCondition<String>() {
+                               @Override
+                               public String apply(WebDriver driver) {
+                                   Set<String> handles = driver.getWindowHandles();
+                                   handles.removeAll(oldWindows1);
+                                   return handles.size() > 0 ? handles.iterator().next() : null;
+                               }
+                           }
+                    );
             Set<String> oldWindows = driver.getWindowHandles();
 
             Iterator ite = oldWindows.iterator();
 
             while (ite.hasNext()) {
                 String popupHandle = ite.next().toString();
-                System.out.println(popupHandle);
+
+
                 if (!popupHandle.contains(originalWindow)) {
                     driver.switchTo().window(popupHandle);
                     driver.close();
